39be28f18d8bd28cefac9a9418c9f66acbcaf734
madisec <madiisec@gmail.com>
2023-12-01 18:36:50 +0330

    Complete tool

---
+++ go.mod
@@ -0,0 +1,9 @@
+module wapp
+
+go 1.21.4
+
+require (
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
+	github.com/projectdiscovery/wappalyzergo v0.0.109 // indirect
+	golang.org/x/net v0.10.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/common-nighthawk/go-figure"
+	wappalyzer "github.com/projectdiscovery/wappalyzergo"
+)
+
+func main() {
+	figure.NewFigure("Wappalizer tool", "rectangles", true).Print()
+	fmt.Println(" 			Wappalizer tool - Powered by MadiSec\n")
+	resp, err := http.DefaultClient.Get(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, _ := ioutil.ReadAll(resp.Body) // Ignoring error for example
+
+	wappalyzerClient, err := wappalyzer.New()
+	fingerprints := wappalyzerClient.Fingerprint(resp.Header, data)
+	fmt.Printf("%v\n", fingerprints)
+
+	// Output: map[Acquia Cloud Platform:{} Amazon EC2:{} Apache:{} Cloudflare:{} Drupal:{} PHP:{} Percona:{} React:{} Varnish:{}]
+}
+
+// package main
+
+// import (
+// 	"fmt"
+
+// 	"github.com/common-nighthawk/go-figure"
+// 	wappalyzer "github.com/projectdiscovery/wappalyzergo"
+// )
+
+// func main() {
+// 	my_figlet := figure.NewFigure("MadiSec", "", true)
+// 	fmt.Println(my_figlet)
+// 	person := map[string]string{"Name": "mehdi", "fullname": "Mehdi Kamari"}
+// 	fmt.Println(person["Name"])
+// }
