8c817783cd44f2fa795916767ac384fd7ad1c194
madisec <madiisec@gmail.com>
2024-02-17 14:18:03 +0330

    Buld version 2: add silent mode

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module wapp
 go 1.21.4
 
 require (
-	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
-	github.com/projectdiscovery/wappalyzergo v0.0.109 // indirect
-	golang.org/x/net v0.10.0 // indirect
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
+	github.com/projectdiscovery/wappalyzergo v0.0.109
 )
+
+require golang.org/x/net v0.10.0 // indirect
--- main.go
+++ main.go
@@ -12,12 +12,32 @@ import (
 )
 
 func main() {
-	figure.NewFigure("Wappalizer tool", "rectangles", true).Print()
-	fmt.Println(" 			Wappalizer tool - Powered by MadiSec")
-	fmt.Println("")
 	url_flag := flag.String("u", "", "Get url for run tool")
+	silent := flag.Bool("silent", false, "Enable silent mnode")
 	flag.Parse()
-	resp, err := http.DefaultClient.Get(*url_flag)
+	if *url_flag != "" && !*silent {
+		fig()
+		wapp(*url_flag)
+	} else if *url_flag != "" {
+
+		wapp(*url_flag)
+	} else {
+		figure.NewFigure("Wappalizer", "larry3d", true).Print()
+		fmt.Println("\tWappalizer tool - Powered by MadiSec")
+		fmt.Println("\tVersion: 2.0.1")
+		fmt.Println("\tFor using script run script with -h flag")
+		fmt.Println("")
+
+	}
+}
+
+func fig() {
+	figure.NewFigure("Wappalizer", "larry3d", true).Print()
+	fmt.Println("\tWappalizer tool - Powered by MadiSec")
+	fmt.Println("")
+}
+func wapp(url string) {
+	resp, err := http.DefaultClient.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,6 +46,4 @@ func main() {
 	wappalyzerClient, err := wappalyzer.New()
 	fingerprints := wappalyzerClient.Fingerprint(resp.Header, data)
 	fmt.Printf("%v\n", fingerprints)
-
-	// Output: map[Acquia Cloud Platform:{} Amazon EC2:{} Apache:{} Cloudflare:{} Drupal:{} PHP:{} Percona:{} React:{} Varnish:{}]
 }
